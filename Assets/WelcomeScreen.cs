43256489f50481370174c31ce5e6a2a2df601783
chibiskye <mongting.gao@stonybrook.edu>
2019-11-16 00:00:36 -0500

    Adding code from high school

---
+++ Assets/WelcomeScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WelcomeScreen : MonoBehaviour {
+
+	void OnGUI() {
+		if (GUI.Button(new Rect(325, 500, 150, 50), "Start")) {
+			Application.LoadLevel("CollectingCoins");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
